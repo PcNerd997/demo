3e5fac412c89ac2174dfe701c826951634c0b1ac
Pc_Nerd <Ajayihabeeb977@gmail.com>
2023-06-23 14:05:27 +0100

    m: testing merging conflict

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ num_a = int(input("Enter the first number: "))
 num_b = int(input("Enter the second number: "))
 sum = num_a + num_b
 print(f"{num_a} + {num_b} = {sum}")
-#find the average of the two number
+#find average of the two number
 avg = sum / 2
 
